4a66c39dd32f90d866e565c5f34821f4ba53b2c6
Carlos Rosario <carlosrosario2k@gmail.com>
2020-08-26 12:52:06 +0000

    Automatically backed up by Learn

---
+++ Album.rb
@@ -0,0 +1,14 @@
+CREATE TABLE cats (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  breed TEXT, 
+  owner_id INTEGER
+  );
+  
+INSERT INTO cats (name, age, breed, owner_id) VALUES ("Maru", 3, "Scottish Fold", 1);
+INSERT INTO cats (name, age, breed, owner_id) VALUES ("Hana", 1 , "Tabby", 1);
+INSERT INTO cats (name, age, breed) VALUES ("Lil\' Bub", 5, "American Shorthair");
+INSERT INTO cats (name, age, breed) VALUES ("Moe", 10, "Tabby");
+INSERT INTO cats (name, age, breed) VALUES ("Patches", 2, "Calico");
+
---
+++ greeting.rb
@@ -0,0 +1,31 @@
+# def greeting(name, language="Ruby")
+# puts "Hello, #{name}. We heard you are a great #{language} programmer."
+# end
+
+# greeting("Sophie", "Ember.js")
+
+class Dog
+  def bark
+    puts 
+    "Woof!"
+  end
+  def sit
+    puts "sit"
+  end
+  
+    
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
+
+
+
+# fido.object_id
+# fido.methods
+
+
+
+
+
